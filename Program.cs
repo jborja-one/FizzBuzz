36417df89401f6441a543d95fdb5e5cb64074ecf
Jonathan Borja <jborja.one@gmail.com>
2024-11-15 21:08:08 -0700

    done

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            // Check if divisible by both 3 and 5
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            // Check if divisible by 3
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            // Check if divisible by 5
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            // Otherwise, just print the number
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
